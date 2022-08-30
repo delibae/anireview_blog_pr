432bb9efa0b4394afbc31db61c0aba930de7e7af
delibae <82585834+delibae@users.noreply.github.com>
2022-08-31 03:13:02 +0900

    화면2 페이지 넘겨서 병원 찾기 기능 추가

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ driver.get("https://map.naver.com")
 time.sleep(2)
 
 search_input = driver.find_element_by_xpath("/html/body/app/layout/div[3]/div[2]/shrinkable-layout/div/app-base/search-input-box/div/div[1]/div/input")
-search_input.send_keys(adress[10] + Keys.ENTER)
-
+search_input.send_keys(adress[4] + Keys.ENTER)
+# search_input.send_keys("경기도 고양시 일산동구 장항동 883번지 호수마을5단지아파트 121호" + Keys.ENTER)
 time.sleep(2)
 try:
     viewmore = driver.find_element_by_class_name("link_more").click()
@@ -50,22 +50,36 @@ except:
 
 
 # 화면2
-# 동물병원 텍스트 찾기
+# 페이지 넘기면서 동물병원 텍스트 찾기
 # try => 링크 클릭
 # except => error code => pass
 
-try:
-    keyword = "동물병원"  
-    # foo 변수를 가진 요소를 찾으려면
-    hospital = driver.find_element_by_xpath("//div[.='" + keyword + "']")
-    for i in range(2):
-        hospital = hospital.find_element_by_xpath('..')
-    time.sleep(3)
-except:
-    print("error2: no animal hospital in the place")
+#to do: 건물에 있는 영업점의수가 많을때
+hospital_exist = 0
+while(1):
+    try:
+        keyword = "동물병원"  
+        # foo 변수를 가진 요소를 찾으려면
+        hospital = driver.find_element_by_xpath("//div[.='" + keyword + "']")
+        for i in range(2):
+            hospital = hospital.find_element_by_xpath('..')
+        hospital.click()
+        time.sleep(3)
+        
+        hospital_exist = 1
+        break
+    except:
+        print("case: this page doesn't contain hospital name")
+    try:
+        next_button = driver.find_element_by_class_name("btn_next")
+        next_button.click()
+        time.sleep(3)
+    except:
+        print("alert: page finish")
+        print("error2: no hospital in the place")
+        break
 
-
-
+time.sleep(2)
 # 화면3(true)
 # 이용 시간 + 전화번호 추출
 # 리뷰 여부 확인
@@ -74,6 +88,26 @@ except:
 # except
 # error code => pass
 
+time_exist = 1
+try:
+    driver.switch_to.frame("entryIframe")
+except:
+    print("error3: iframe switch error")
+    time_exist = 0
+try:
+    
+    time_not_exist = "이용시간을 알려주세요."
+    time_n_exist = driver.find_element_by_xpath("//span[.='" + time_not_exist + "']")
+    if time_n_exist != None:
+        time_exist = 0
+        print(time_exist)
+        print("exception1: no operationg time")
+    
+except:
+    print(time_exist)
+
+if time_exist == 1:
+    
 driver.close()
 
 # 화면4(true)
